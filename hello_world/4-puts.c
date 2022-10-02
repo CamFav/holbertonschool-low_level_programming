f255d6a89610e08b5310bd384d1ee6ee8d43b3ab
Meashyshy <camillefvrl@gmail.com>
2022-10-02 05:44:25 -0700

    Modificiation

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - used to prints "Programming is like building a multilingual puzzle 
- * Return: 0
+ * main - Entry point
+ *
+ * Return: 0 if success
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+
+	return (0);
 }
