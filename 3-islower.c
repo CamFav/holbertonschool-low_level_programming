56c4b14e4fc9cd7d2212817ee32b3cd414bc3117
Meashyshy <camillefvrl@gmail.com>
2022-10-08 14:41:31 -0700

    commit

---
+++ 3-islower.c
@@ -0,0 +1,17 @@
+/**
+ * _islower - description
+ * main - description
+ * @c: Letter to check case of
+ *
+ * Return: Always 1, return 0 if not lower
+ */
+	int main(void)
+{
+	int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	return (1);
+		else
+	return (0);
+}
+}
