9fa21c2795683459447da81964eb3693011158eb
Meashyshy <camillefvrl@gmail.com>
2022-10-08 14:18:15 -0700

    commit

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - description
+ *
+ * Return: Always 0
+ */
+	int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+{
+	putchar(n);
+i	if (n < '9')
+{
+	putchar (',');
+	putchar (' ');
+}
+	n++;
+}
+	putchar ('\n');
+	return (0);
+}
