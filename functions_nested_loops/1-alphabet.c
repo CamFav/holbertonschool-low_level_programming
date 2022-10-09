247441fedbebf23f879d9c8cecb05ecb3f52f7d3
Meashyshy <camillefvrl@gmail.com>
2022-10-09 07:03:26 -0700

    commit

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - description
+ *
+ * Return: Always 0
+ */
+	void print_alphabet(void)
+{
+	char l = 'a';
+	
+	while (l <= 'z')
+{
+	_putchar(l);
+	l++;
+}
+	_putchar('\n');
+}
