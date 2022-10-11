cc32401d5adfccdf099c28b26d227633cfa0604f
Meashyshy <camillefvrl@gmail.com>
2022-10-11 10:31:03 -0700

    commit

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - description
+ * @n: input
+ *
+ * Return: none
+ */
+	void print_to_98(int n)
+{
+	while (n != 98)
+{
+	printf("%i", n);
+	if (n > 98)
+		n--;
+	else
+		n++;
+}
+	printf("98\n");
+}
