48895f210d66feeb01b051ef43aec014e12eba7e
Meashyshy <camillefvrl@gmail.com>
2022-10-11 05:30:42 -0700

    commit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,8 +7,12 @@
  */
 	int print_last_digit(int n)
 {
-	if ((n % 10) < 0)
-		n *= -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	int a, b;
+
+	a = n % 10;
+if (a < 0)
+	a = -1;
+b = '0' + a;
+	_putchar(b);
+return (a);
 }
