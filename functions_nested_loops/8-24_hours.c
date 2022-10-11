ff7ac617f22754f04b5407620edefcc1a40c5acf
Meashyshy <camillefvrl@gmail.com>
2022-10-11 06:17:43 -0700

    commit

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,10 +6,10 @@
  */
 	void jack_bauer(void)
 {
-	int h = 0, m = 0;
+	int h = 0;
 for(;h < 24; h++)
 {
-for(;m < 60; m++)
+for(m = 0; m < 60; m++)
 {
 	_putchar((h / 10) + '0');
 	_putchar((h % 10) + '0');
