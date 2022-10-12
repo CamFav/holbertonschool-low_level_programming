cf9987f17c7768e1ef324dc28e5e2a76b6163b1c
Meashyshy <camillefvrl@gmail.com>
2022-10-12 13:22:19 -0700

    commit

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,13 +9,13 @@
 	void print_to_98(int n)
 {
 if (n >= 98)
-{	
+{
 for (; n >= 98; n--)
 {
 printf("%i", n);
 if (n != 98)
 printf(", ");
-} 
+}
 printf("\n");
 }
 else
@@ -25,7 +25,7 @@ for (; n <= 98; n++)
 printf("%i", n);
 if (n != 98)
 printf(", ");
-}	
+}
 printf("\n");
 }
 }
