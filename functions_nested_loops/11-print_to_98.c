6f5f748f199cae9bf82092a10165f6508dc2ef99
Meashyshy <camillefvrl@gmail.com>
2022-10-12 08:41:09 -0700

    commit

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,17 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_to_98 - description
  * @n: input
  *
  * Return: none
  */
-	void print_to_98(int n)
-{
-	for (n = 0; n < 98; n++)
-{
+	void print_to_98(int n) {
+if (n >= 98)
+	for (; n >= 98; n++)
+	printf("%i, ", n)
+else
+	for (; n <= 98; n++)
 	printf("%i, ", n);
+	printf("\n");
 }
-	printf("98\n");
+	
+
+	
 }
 
