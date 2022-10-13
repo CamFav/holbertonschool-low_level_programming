6162fc68fae6c98310617a5c9d7ac43203570820
Meashyshy <camillefvrl@gmail.com>
2022-10-13 11:00:58 -0700

    Adding files

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * swap_int - Function that swaps the values of two integers.
+ * @a: input
+ * @b: input
+ * @c: input
+ *
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -14,4 +14,5 @@ int add(int, int);
 void print_to_98(int);
 void print_times_table(int);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 #endif
