76bb833f1bb57fda28ba5510d22a7677184382ae
Meashyshy <camillefvrl@gmail.com>
2022-10-14 05:19:31 -0700

    Adding files

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - function that concatenates two strings 
+ * @dest: string to src
+ * @src: string to dest
+ *
+ * Return: Always 0.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a, b;
+
+	a = b = 0;
+	while (dest[a])
+		i++;
+	while (dest[a] = src[b])
+{
+	a++;
+	b++;
+}
+	return (dest);
+}
