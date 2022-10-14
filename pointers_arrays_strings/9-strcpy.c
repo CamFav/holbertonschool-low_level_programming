bfb0c73131de8e3026a0e7519acd60578b244c96
Meashyshy <camillefvrl@gmail.com>
2022-10-14 03:57:55 -0700

    Adding files

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy - function that copies pointed string by src to pointed dest
+ * @src: source
+ * @dest: destination
+ *
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+{
+	dest[i] = src[i];
+	i++;
+}
+	return (dest);
