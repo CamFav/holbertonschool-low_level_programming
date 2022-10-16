53bd761e00d6db869dfb3bdb3b4a51d011569681
Meashyshy <camillefvrl@gmail.com>
2022-10-16 11:00:26 -0700

    commit

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,7 @@ char *cap_string(char *s)
 {
 	int i = 0;
 	int j = 0;
-	char *a = ",.;{}()\"?!\n\t ";
+	char [a] = ",.;{}()\"?!\n\t ";
 
 while (s[i] != 0)
 {
@@ -17,7 +17,7 @@ if (s[i] >= 97 && s[i] <= 122)
 {
 	if (i == 0)
 		s[i] -= 32;
-	else if (s[i - 1] == a[j])
+	else if (s[i - 1] == a[j]) 
 		s[i] -= 32;
 }
 i++;
