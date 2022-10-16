7d66625fb1258d57a6a5f7ea39eb86673a904c4a
Meashyshy <camillefvrl@gmail.com>
2022-10-16 08:21:41 -0700

    adding files

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+include "main.h"
+/**
+ * string_toupper - function that lowercases to uppercase letters
+ * @s: string
+ *
+ * Return: String with uppercase letters
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+while (s[i] != 0)
+{
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - 32;
+	i++;
+}
+return (s);
+}
+
