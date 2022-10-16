40c6d8ea8335a5ea0a622e57c3f8798cbdd1b235
Meashyshy <camillefvrl@gmail.com>
2022-10-16 06:38:17 -0700

    Adding file

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strncpy - function that copies a string
+ * @dest: string to copy
+ * @src: copied string
+ * @n: chars to copy
+ *
+ * Return: dest 
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+
--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,6 +7,22 @@
  */
 void rev_string(char *s)
 {
+	int a = 0, b = 0;
+	char str[50]
+	
+	while (s[a])
+{
+	str[a] = s[a];
+	a++;
+}
+	a = a - 1;
+	while (a >= 0)
+{
+	s[a] = str[b];
+	a++;
+	b--;
+}
+}
 
 
 
