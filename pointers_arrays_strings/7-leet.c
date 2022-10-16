7c7a4c9f0baaced2d2cd4b4dae3f5c76fdd60751
Meashyshy <camillefvrl@gmail.com>
2022-10-16 09:41:24 -0700

    adding files

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -6,8 +6,25 @@
  *
  *
  */
-char *leet(char *n)
+char *leet(char *s)
 {
+	int i, j;
+	i = j = 0
+
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+while (s[i] != 0)
+{
+	while (a[j] != '\0')
+{
+	if (s[i] == a[j])
+		s[i] = b[j];
+	j++;
+}
+i++;
+}
+return (s);
+}
 
 
 
