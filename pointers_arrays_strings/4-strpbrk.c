a17e9df726ad6fbd26aa5ee0acb252339bc446a6
Meashyshy <camillefvrl@gmail.com>
2022-10-16 14:27:05 -0700

    Adding files

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strpbrk - function that finds the first character
+ * in the s1 matching with character in s2
+ * @s: scanned string
+ * @accept: bytes to search
+ *
+ * Return: first matching character
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i;
+
+while (s[i] != 0)
+{
+	i = 0;
+while (accept[i] != '\0')
+{
+	if (accept[i] == s[i])
+return (s);
+	i++;
+}
+	s++;
+}
+return (0);
+}
