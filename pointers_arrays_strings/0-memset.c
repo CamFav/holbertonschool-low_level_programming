a6427b7f099b191d65480bb5bbec91e9c4677b9c
Meashyshy <camillefvrl@gmail.com>
2022-10-16 12:34:43 -0700

    Adding files

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset - function that fills memory with a constant byte
+ * @s: memory space
+ * @n: number, size of spaces to fill
+ * @b: byte to fill
+ *
+ * Return: Pointer to memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	i = 0;
+
+while (i < n)
+{
+	s[i] = b;
+	i++;
+}
+return (s);
+}
