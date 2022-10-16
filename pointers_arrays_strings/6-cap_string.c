052565a804f073f829706020ad84b4731c65c337
Meashyshy <camillefvrl@gmail.com>
2022-10-16 10:34:21 -0700

    fix error

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,17 +8,22 @@
 char *cap_string(char *s)
 {
 	int i = 0;
+	int j = 0;
+	char a[] = " \t\n,;.!?"(){}";
 if (s[i] >= 97 && s[i] <= 122)
 {
 	s[i] -= 32;
 }
 for (i = 1; s[i] != '\0'; i++)
 {
+	if (s[i] == a[j])
+{
 	if (s[i - 1] == 32)
 {
 	if (s[i] >= 97 && s[i] <= 122)
 {
 	s[i] -= 32;
+}
 }
 }
 }
