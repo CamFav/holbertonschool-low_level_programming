600c46ef9fcadb7fc3d37fa2dbc1dead9432d910
Meashyshy <camillefvrl@gmail.com>
2022-10-16 14:09:33 -0700

    Fix error

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -9,9 +9,20 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0;
-while (*s++ && *accept++)
-	;
-
+	unsigned int i, j;
+for (i = 0; s[i]; i++)
+{
+	for (j = 0; accept[j]; j++)
+{
+	if (s[i] == accept[j])
+{
+	break;
+}
+}
+	if (s[i] != accept[j])
+{
+	break;
+}
+}
 return (i);
 }
