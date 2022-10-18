94ca0b205e1b26c8354c2edfadff07a8bcccf305
Meashyshy <camillefvrl@gmail.com>
2022-10-18 15:03:05 -0700

    Fixing errors

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _pow_recursion - function that returns the factiorial of a given number
+ * factorial - function that returns the factiorial of a given number
  * @n: integer
  *
  * Return: Factorial of n, or -1 if lower than 0.
@@ -11,7 +11,5 @@ int factorial(int n)
 		return (-1);
 	if (n <= 1)
 		return (1);
-	return (n * factorial(n -1));
+	return (n * factorial(n - 1));
 }
-
-
