a86d5dd69e5b9775a92f5afe9a99a32d0d7b5fd4
Meashyshy <camillefvrl@gmail.com>
2022-10-18 14:25:15 -0700

    Fixing syntaxes

--- recursion/main.h
+++ recursion/main.h
@@ -1,4 +1,3 @@
-#define MAIN_H
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -13,4 +12,3 @@ int add(int, int);
 void print_to_98(int);
 void print_times_table(int);
 void _puts_recursion(char *s);
-#endif
