0db8e1a1133f2494dd743c4c1048b7301d36c80c
Meashyshy <camillefvrl@gmail.com>
2022-10-21 14:54:06 -0700

    Adding files

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints the name of the program
+ * @argc: arguments number
+ * @argv: arguments vector
+ *
+ * Return: name of the program
+ */
+int main(int __attribute__((unused)), char *argv[])
+{
+	printf("%\n", argv[0]);
+	return (0);
+}
