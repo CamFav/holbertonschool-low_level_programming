10952e545e78cb96936c8f7de4cdaadce8909f49
Meashyshy <camillefvrl@gmail.com>
2022-10-27 06:08:25 -0700

    Adding files

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - function that allocates memry using malloc
+ * @b: memory
+ *
+ * Return: Pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
