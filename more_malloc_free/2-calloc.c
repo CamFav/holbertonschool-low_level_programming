0b5f2ad14d087dead83351981e8a05f314fc4d95
Meashyshy <camillefvrl@gmail.com>
2022-10-28 14:58:48 -0700

    Fixing error

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -17,18 +17,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	c = malloc(nmemb * size);
-	if (c = NULL)
+	if (c == NULL)
 		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
 		c[i] = 0;
 	return (c);
-}
-
-
-
-
-
-
-
-
 }
