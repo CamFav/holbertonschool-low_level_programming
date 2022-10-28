fcd36fbb0cb0918e4ca95b6c19792130e9671c1c
Meashyshy <camillefvrl@gmail.com>
2022-10-28 09:58:08 -0700

    Adding function to main

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - function that allocates memory for an array, using 
+ * malloc
+ * @nmemb:
+ * @size:
+ *
+ * Return: Nothing.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+
+
+
+
+
+
+
+}
---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_range - function that cretes an array of integers 
+ * @min:
+ * @max:
+ *
+ * Return: Nothing.
+ */
+int *array_range(int min, int max)
+{
+
+
+
+
+
+}
