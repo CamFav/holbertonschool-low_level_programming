ccf2261a7a9f2b13fb84bdffe4e8c865368fa293
Meashyshy <camillefvrl@gmail.com>
2022-11-04 12:08:26 -0700

    Fixing error

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -7,16 +7,17 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
-{
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
-}
-	else
+	if (d == NULL)
 {
 	printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+}
 	printf("Age: %f\n", d->age);
+	if (d == NULL)
+{
 	printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
 }
