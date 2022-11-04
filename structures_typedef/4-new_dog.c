37c43d9a8c2b7ba63e0012dcb22794c79715af69
Meashyshy <camillefvrl@gmail.com>
2022-11-04 14:16:54 -0700

    Please

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,9 +13,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	struct User *new_dog;
 	new_dog = malloc(sizeof(dog_t));
 
-	if (new_dog == NULL)
-		return (NULL);
+	strcpy(*new_dog.name);
+	strcpy(*new_dog.owner);
+
 	new_dog->name = name;
 	new_dog->age = age;
 	new_dog->owner = owner;
+	
+	if (new_dog == NULL)
+	return (NULL);
 }
