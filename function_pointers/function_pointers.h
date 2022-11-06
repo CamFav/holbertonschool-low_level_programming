598c6dc10a53c01a1dcc3c128811407d60d16581
Meashyshy <camillefvrl@gmail.com>
2022-11-06 12:29:37 -0800

    Fixing syntaxe

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,5 +2,5 @@
 #define FUNCTION_POINTERS_H
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *s));
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size, void (*action)(int));
 #endif
