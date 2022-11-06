d0c17e613eaa9c32734664bd2a72354f450443d5
Meashyshy <camillefvrl@gmail.com>
2022-11-06 12:56:02 -0800

    Fixing syntaxes

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -11,10 +11,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
-	if (!array | size <=0)
-		return (-1);
-	if (!cmp)
-		return (-1);
+	if (!action | !array)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i])
 }
