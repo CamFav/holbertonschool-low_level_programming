5b91c8341e2fd94c66c4ab1e4f7917b93667a1e7
Meashyshy <camillefvrl@gmail.com>
2022-11-06 14:02:03 -0800

    Adding files

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -7,5 +7,5 @@ void print_name(char *name, void (*f)(char *s));
 void array_iterator(int *array, size_t, void (*action)(int));
 int int_index(int *array, int size, int(*cmp)(int));
 int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsgined int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
