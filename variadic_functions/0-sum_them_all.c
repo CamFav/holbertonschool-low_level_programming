df4db00052514a184f16ceee7891dd3c4e0791e1
Meashyshy <camillefvrl@gmail.com>
2022-11-06 13:30:13 -0800

    Adding file

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -14,11 +14,11 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(ap, n);
 
-	a = 0;
+	sum = 0;
 	
 	for (i = 0; i < n; i++)
 	{
-		a += va_arg(ap, int);
+		sum += va_arg(ap, int);
 	}
 va_end(ap);
 return n;
