c4338cda74ed6f9f8ea8f5f7454ef13b019058db
Meashyshy <camillefvrl@gmail.com>
2022-11-06 12:28:18 -0800

    Adding file

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -11,7 +11,7 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i = 0;
 
 	if (!array | !cmp)
 		return (-1);
@@ -19,10 +19,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
-	for (i = 0 < size; i++)
+	for (; i < size; i++)
 {
 		if (cmp(array[i]))
-			return(i);
+			return (i);
+		i++;
 }
 return (-1);
 }
