4a4adde5251623f312a37a1db6e7ed87a4d6ea15
Meashyshy <camillefvrl@gmail.com>
2022-11-06 14:00:16 -0800

    Adding files

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,10 +1,11 @@
-#include <stddef.h>
-#include <stdarg.h>
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stdio.h>
+#include <stdarg.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *s));
 void array_iterator(int *array, size_t, void (*action)(int));
 int int_index(int *array, int size, int(*cmp)(int));
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsgined int n, ...);
 #endif
