560546b970087412b099c1fc10d0fa8b35371ef2
Meashyshy <camillefvrl@gmail.com>
2022-11-15 06:34:24 -0800

    Adding directory

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -10,14 +10,14 @@
 int sum_them_all(const unsigned int n, ...)
 {
 
-	va_list ap;
+	va_list ap; // Initialize the va_list structure, used for argument pointer variables.
 	unsigned int i;
 	int sum = 0;
 
-	va_start(ap, n);
+	va_start(ap, n); // Initialize the argument pointer variable ap to point to the first of the optional arguments of the current function; last must be the last required argument to the function
 
-	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
-	va_end(ap);
+	for (i = 0; i < n; i++) // For all unnamed integer, do an addition until it reach int n
+		sum += va_arg(ap, int); // va_arg returns the value of the next parameter 
+	va_end(ap); // Cleans up the list (void va_end (va_list ap)
 	return (sum);
 }
---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_all - function that prints anything
+ * @format: list of types of arguments
+ * @c: char
+ * @i: integer
+ * @f: float
+ * @s: char *
+ *
+ */
+
+void print_char(char c)
+{
+
+}
+
+
+
+void print_all(const char * const format, ...)
+{
+
+
+
+
+}
