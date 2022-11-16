2af1e41334bc9dcfe3edfc32cc4a9a15a5771119
Meashyshy <camillefvrl@gmail.com>
2022-11-16 06:30:40 -0800

    Adding While loop

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,6 +9,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newNode = malloc(sizeof(list_t));
+	unsigned int i = 0;
 
 	if (!newNode)
 		return (NULL);
@@ -16,6 +17,8 @@ list_t *add_node(list_t **head, const char *str)
 	newNode->str = strdup(str); 
 	newNode->next = *head;
 	*head = newNode;
-	return (newNode);
 
+	while (str[i])
+		i++;
+	return (newNode);
 }
