715a3e8d136b72ead8d600c9c05cd6e0a130333f
Meashyshy <camillefvrl@gmail.com>
2022-11-16 06:14:53 -0800

    Adding IF Fail, Return NULL

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,8 +10,13 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newNode = malloc(sizeof(list_t));
 
+	if (!newNode)
+		return (NULL);
+
 	newNode->str = strdup(str);
+	newNode->len = 
 	newNode->next = *head;
 	*head = newNode;
 	return (newNode);
+
 }
