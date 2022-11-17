b7e01dcd3fe8182346e525ddf45809cbbbbd0eba
Meashyshy <camillefvrl@gmail.com>
2022-11-17 13:16:04 -0800

    Free

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - function that frees a list
+ * @head: pointer to head node
+ *
+ * Return: Nothing.
+ */
+void free_listint(listint_t *head)
+{
+	if (!head)
+		return;
+	if (head->next)
+		free_listint(head->next);
+
+	free(head->n);
+	free(head);
+}
