5b20b3cbb35629abfbbadb67d7d28d90fb76f42e
Meashyshy <camillefvrl@gmail.com>
2022-11-17 12:59:22 -0800

    Fixed integer call

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -14,7 +14,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (NULL);
 
 	newNode->next = *head;
-	newNode->n = strlen(n);
+	newNode->n = n;
 	*head = newNode;
 
 	return (newNode);
