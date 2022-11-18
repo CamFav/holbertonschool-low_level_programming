13365f96f643844df23402a12be8cae7f2b4832f
Meashyshy <camillefvrl@gmail.com>
2022-11-18 03:06:05 -0800

    Adding function prototype

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * pop_listint - function that deletes the head node of a linked list
+ * and returns the head node's data.
+ * @head: pointer to head node
+ *
+ * Return: data (n)
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *newNode;
+	int n;
+
+	if (!head)
+		return (0);
+
+	n = pop_listint(&head)
+
+	newNode = *head;
+
+	*head = newNode->next;
+	free(newNode);
+	return (n);
+}
