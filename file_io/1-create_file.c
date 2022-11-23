c2b1c00219a7414b780df98bb80013dcacee280a
Meashyshy <camillefvrl@gmail.com>
2022-11-23 04:52:10 -0800

    Added permissions to created file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file_descriptor == -1)
 		return (-1);
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	write(file_descriptor, text_content, strlen(text_content));
 	close(file_descriptor);
-	return (1);	
+	return (1);
 }
 
 
