3a2d2741e49c24dbb8bdb59fcf09f26cc8fa61e8
Meashyshy <camillefvrl@gmail.com>
2022-11-23 04:38:55 -0800

    Checker Correction

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor, index, fprint;
+	int file_descriptor;
 
 	if (!filename)
 		return (-1);
@@ -23,6 +23,7 @@ int create_file(const char *filename, char *text_content)
 		close(file_descriptor);
 		return (1);
 	}
+	return (0);
 }
 
 
