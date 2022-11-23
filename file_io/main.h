474a77485912b75e91e36b725a62f569b4401027
Meashyshy <camillefvrl@gmail.com>
2022-11-23 02:47:30 -0800

    Updating library functions

--- file_io/main.h
+++ file_io/main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stats.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
