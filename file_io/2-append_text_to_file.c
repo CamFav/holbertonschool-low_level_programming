bd7b503fdf24ead9e2124f4570eb810238b83b99
Meashyshy <camillefvrl@gmail.com>
2022-11-23 05:12:27 -0800

    Added text to append

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -8,8 +8,22 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file_descriptor, new_string;
+	int file_descriptor, i;
 
 	if (!filename)
 		return (-1);
+
+	file_descriptor = open(filename, OWRONLY | O_APPEND);
+
+	if (file_descriptor == -1)
+		return (-1);
+
+	if (!text_content)
+		return (1);
+
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+	write(file_descriptor, text_content, i);
+	close(file_descriptor);
+	return (1);
 }
