9747b08699ecaa8160bc4b610101fcbeb2831e66
Meashyshy <camillefvrl@gmail.com>
2022-11-24 02:50:32 -0800

    Adding function

---
+++ file_io/3-cp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - copies the content of a file to another file.
+ * @ac: number of arg
+ * @av: the arg
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+
+
+
+}
