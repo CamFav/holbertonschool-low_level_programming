02a94692d4eb62339938799e5094726908f9a65e
Meashyshy <camillefvrl@gmail.com>
2022-11-25 04:32:23 -0800

    Deleted all the IF by a single FOR

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,21 +1,19 @@
 #include "lists.h"
 /**
- * print_dlistint - function that prints all the elements of a doubly linked list.
+ * print_dlistint - function that prints all the elements of
+ * a doubly linked list.
  * @h: pointer to head node
  *
  * Return: number of elements
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t to_next = 1;
+	size_t elements = 0;
 
-	if (!h)
-		return (0);
-	if (h->n)
+	for (; h; elements++)
+	{
 		printf("%d\n", h->n);
-	if (h->prev)
-		return (0);
-	if (h->next)
-		to_next = to_next + print_dlistint(h->next);
-	return (to_next);
+		h = h->next;
+	}
+	return (elements);
 }
