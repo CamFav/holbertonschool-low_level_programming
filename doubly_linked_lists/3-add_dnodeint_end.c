12af6cd99d82ec9d6b83b22efe9781e405f4b29b
Meashyshy <camillefvrl@gmail.com>
2022-11-25 07:29:28 -0800

    Fixed guards

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -27,9 +27,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		dlistint_t *lastNode = *head;
 	
 		while (lastNode->next)
+		{
 			lastNode = lastNode->next;
 			lastNode->next = newNode;
 			return (newNode);
+		}
 	}
 }
-
