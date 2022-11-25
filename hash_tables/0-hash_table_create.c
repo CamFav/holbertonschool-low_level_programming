ad26deaccfba48ed3f8490568dd7484b821ffa4c
Meashyshy <camillefvrl@gmail.com>
2022-11-25 13:38:24 -0800

    Adding C - Hash Tables

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash.h"
+/**
+ * hash_table_create - function that creates a hash table.
+ * @size: size of the array
+ *
+ * Return: pointer to new hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *newHash;
+	hash_table_t **newNode;
+
+	newHash = malloc(sizeof(hash_table_t));
+	if (!newHash)
+		return (NULL);
+
+	newHash->size = size;
+	newNode = malloc(sizeof(hash_t *) * size);
+
+	if (!newNode)
+	{
+		free(newHash);
+		return (NULL);
+	}
+	newHash->array = newNode;
+
+	return (newHash);
+}
