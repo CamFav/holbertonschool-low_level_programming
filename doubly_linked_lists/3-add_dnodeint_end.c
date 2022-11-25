149154514d8291c063ebaa3cb946112c01a5542e
Meashyshy <camillefvrl@gmail.com>
2022-11-25 07:28:40 -0800

    Added function prototype

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * add_dnodeint - function that adds a new node at the end of
+ * a doubly linked list.
+ * @head: pointer to head node
+ * @n: integer
+ *
+ * Return: EXIT_SUCCESS.
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *newNode = malloc(sizeof(dlistint_t));
+
+	if (!newNode)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->prev = NULL;
+
+	if (!(*head))
+	{
+		*head = newNode;
+		return (newNode);
+	}
+	else
+	{
+		dlistint_t *lastNode = *head;
+	
+		while (lastNode->next)
+			lastNode = lastNode->next;
+			lastNode->next = newNode;
+			return (newNode);
+	}
+}
+
