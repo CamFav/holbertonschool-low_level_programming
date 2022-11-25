2d0b2332d7d9f73ce1e6cbba884fc5d2f3e5e75d
Meashyshy <camillefvrl@gmail.com>
2022-11-25 07:17:28 -0800

    Fixed variable declarations

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -9,8 +9,10 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *newNode = malloc(sizeof(dlistint_t));
+	dlistint_t *newNode;
+
 	newNode = *head;
+	newNode = malloc(sizeof(dlistint_t));
 
 	if (!newNode)
 		return (NULL);
