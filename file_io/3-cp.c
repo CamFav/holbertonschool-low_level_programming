0d61813cb5669cf3e57783649a206a2f0f9b578b
Meashyshy <camillefvrl@gmail.com>
2022-11-25 03:54:36 -0800

    Updated whole code

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,53 +8,49 @@
  */
 int main(int ac, char **av)
 {
-	int file_descriptor, copy_file;
+	int file_descriptor, copy_file, fread, fprint;
 	char buffer[1024];
 
 	if (ac != 3)
-		exit();
-
-	file_descriptor = 0;
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 
 	file_descriptor = open(av[1], O_RDONLY);
 
 	if (file_descriptor == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
-	}
 
-	copy_file = 0;
+	copy_file = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	copy_file = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (copy_file == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
 
-	size = 1;
+	fread = 1;
 
-	while (size > 0)
+	while (fread)
 	{
-		size = read(file_descriptor, buffer, 1024);
-		if (size == -1)
-		{
+		fread = read(file_descriptor, buffer, 1024);
+		if (fread == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
-		}
-		fprint = write(copy_file, buffer, size);
-		fprint = 0;
-
-		if (fprint == -1)
+		if (fread > 0)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			exit(99);
+			fprint = write(copy_file, buffer, fread);
+			if (fprint != fread || fprint == -1)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+				exit(99);
 		}
 	}
-
-	if (close(file_descriptor) == -1)
-	{
+	if (close (file_descriptor) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_descriptor);
 		exit(100);
-	}
 	if (close(copy_file) == -1)
-	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", copy_file);
 		exit(100);
-	}
 	return (0);
 }
+		
+
+
