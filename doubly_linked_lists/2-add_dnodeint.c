af1e94672f1b5ef0b64371d4e67ce5de182d17df
Meashyshy <camillefvrl@gmail.com>
2022-11-25 07:14:10 -0800

    Deleted wrong code

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,11 +10,9 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newNode = malloc(sizeof(dlistint_t));
+	newNode = *head;
 
 	if (!newNode)
-		return (NULL);
-
-	if (!head)
 		return (NULL);
 
 	newNode->next = *head;
