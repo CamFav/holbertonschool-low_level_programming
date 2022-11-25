077ec22d3b031a8c74423d02353992c2277aba03
Meashyshy <camillefvrl@gmail.com>
2022-11-25 09:38:07 -0800

    Fixed syntaxe

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (*h)
 	{
 		previousNode = NULL;
-		while (currentNode->previousNode)
+		while (currentNode->prev)
 			currentNode = currentNode->prev;
 		for (count = 0; currentNode && count < idx; count++)
 		{
