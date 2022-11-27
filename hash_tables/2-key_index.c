b75fb21c8c0b715df4329611e4e4deb5e11d9e12
Meashyshy <camillefvrl@gmail.com>
2022-11-27 09:02:43 -0800

    Added function - saved changes

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+#include "hash_tables.c"
+/**
+ * key_index - function that gives you the index of a key
+ * @key: value
+ * @size: size of the array of the hash table
+ *
+ * Return: Storage index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+}
