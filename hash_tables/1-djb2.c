bc55756792ad89a92b3f2fffdf749ac9534226a9
Meashyshy <camillefvrl@gmail.com>
2022-11-27 08:56:11 -0800

    Saved changes

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 - function implementing the djb2 algorithm
+ * @str: strings
+ *
+ * Return: EXIT_SUCCESS
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+
