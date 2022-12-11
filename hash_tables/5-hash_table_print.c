9faca096c8b5770f9e6a78c3a2e6a133f5abe078
Meashyshy <camillefvrl@gmail.com>
2022-12-11 11:27:45 -0800

    Checker Test

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,15 @@
+#include "hash_tables.c"
+/**
+ * hash_table_print - function that prints a hash table.
+ * @ht: hash table
+ *
+ * Return: EXIT_SUCESS.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	if (!ht)
+	{
+		return (NULL);
+	}
+	return (0);
+}
