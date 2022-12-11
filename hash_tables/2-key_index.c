884d5dd2c4d9de0d7445b0f3d79f2dfa1efdac53
Meashyshy <camillefvrl@gmail.com>
2022-12-11 10:44:47 -0800

    Betty Correction

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,4 +1,4 @@
-#include "hash_tables.c"
+#include "hash_tables.h"
 /**
  * key_index - function that gives you the index of a key
  * @key: value
@@ -8,4 +8,5 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+	return (hash_djb2(key) % size);
 }
