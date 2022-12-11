3a5b67e3357c57b5f157965676acac330aeec649
Meashyshy <camillefvrl@gmail.com>
2022-12-11 10:52:15 -0800

    Added function

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - function that retrieves a value associated with a key.
+ * @ht: hash table
+ * @key: key
+ *
+ * Return: Element value, or NULL if key is not found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+
