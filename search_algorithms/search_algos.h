be71ac9ebc63d575ecd2f6af3065fdee51028022
Meashyshy <camillefvrl@gmail.com>
2023-01-04 11:49:54 -0800

    Added header file

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+#include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <strings.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
